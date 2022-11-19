d255120373f2df85532007c8413d9a77a28b9126
Bodzisz <kacpiw01pol@gmail.com>
2022-11-20 00:18:41 +0100

    Add db init sql script

---
+++ resources/init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS ingredients (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  description TEXT,
+  isAlcoholic BOOLEAN NOT NULL CHECK(isAlcoholic IN (0, 1)),
+  strABV INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS drinks (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  isAlcoholic BOOLEAN NOT NULL CHECK(isAlcoholic IN (0, 1)),
+  instructions TEXT
+);
+
+CREATE TABLE IF NOT EXISTS drinks_ingredients (
+  drink_id INTEGER NOT NULL,
+  ingredient_id INTEGER NOT NULL,
+  PRIMARY KEY (drink_id, ingredient_id),
+  FOREIGN KEY (drink_id) REFERENCES drinks(id),
+  FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+);
