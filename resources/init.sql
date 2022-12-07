b1e49436473979c8683768215a018449984fcc20
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2022-12-07 11:38:40 +0100

    New db scheme

--- resources/init.sql
+++ resources/init.sql
@@ -1,22 +1,22 @@
-CREATE TABLE IF NOT EXISTS ingredients (
+CREATE TABLE drinks (
   id INTEGER NOT NULL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
-  description TEXT,
-  isAlcoholic INTEGER NOT NULL CHECK(isAlcoholic IN (0, 1)),
-  strABV INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS drinks (
-  id INTEGER NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE,
-  isAlcoholic INTEGER NOT NULL CHECK(isAlcoholic IN (0, 1)),
+  isAlcoholic BOOLEAN NOT NULL CHECK(isAlcoholic IN (0, 1)),
   instructions TEXT
 );
 
-CREATE TABLE IF NOT EXISTS drinks_ingredients (
+CREATE TABLE ingredients (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  isAlcoholic BOOLEAN NOT NULL CHECK(isAlcoholic IN (0, 1)),
+  strABV INTEGER
+);
+
+CREATE TABLE drinks_ingredients (
+  drinks_ingredients_id INTEGER PRIMARY KEY,
   drink_id INTEGER NOT NULL,
   ingredient_id INTEGER NOT NULL,
-  PRIMARY KEY (drink_id, ingredient_id),
+  ingredient_measure TEXT NOT NULL,
   FOREIGN KEY (drink_id) REFERENCES drinks(id),
   FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
 );
