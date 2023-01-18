8bc413fb46a699c28350211eec69e9749cd95427
jansk <janskibinski2001@gmail.com>
2023-01-18 11:35:28 +0100

    Added unit test and some refactoring.

--- app/src/test/java/io/github/drinkfinder/UtilityFunctionsUnitTest.kt
+++ app/src/test/java/io/github/drinkfinder/UtilityFunctionsUnitTest.kt
@@ -1,67 +1,68 @@
 package io.github.drinkfinder
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 
-class ExampleUnitTest {
+class UtilityFunctionsUnitTest {
     @Test
     fun returnsCorrectName() {
-        assert(UtilityFunctions.getImageName("abilene")=="abilene")
+        assertTrue(UtilityFunctions.getImageName("abilene")=="abilene")
     }
     @Test
     fun returnsCorrectWhenEmpty() {
-        assert(UtilityFunctions.getImageName("")=="")
+        assertTrue(UtilityFunctions.getImageName("")=="")
     }
     @Test
     fun returnsCorrectWhenNumbers() {
-        assert(UtilityFunctions.getImageName("155 belmont")=="_55_belmont")
+        assertTrue(UtilityFunctions.getImageName("155 belmont")=="_55_belmont")
     }
     @Test
     fun returnsCorrectWhenSymbols() {
-        assert(UtilityFunctions.getImageName("50/50")=="_0_50")
+        assertTrue(UtilityFunctions.getImageName("50/50")=="_0_50")
     }
     @Test
     fun returnsCorrectWhenNonExistent() {
-        assert(UtilityFunctions.getImageName("smiesznee nazwy")=="smiesznee_nazwy")
+        assertTrue(UtilityFunctions.getImageName("smiesznee nazwy")=="smiesznee_nazwy")
     }
     @Test
     fun returnsCorrectConcat() {
         val result =  UtilityFunctions.concatNextItem("test", "Fajna rzec")
-        assert(result == "test\n- Fajna rzec")
+        assertTrue(result == "test\n- Fajna rzec")
     }
     @Test
     fun returnsCorrectConcatWhenToContactEmpty() {
         val result =  UtilityFunctions.concatNextItem("", "Fajna rzec")
-        assert(result == "- Fajna rzec")
+        assertTrue(result == "- Fajna rzec")
     }
     @Test
     fun returnsCorrectConcatWhenItemEmpty() {
         val result =  UtilityFunctions.concatNextItem("fajnarzecz", "")
-        assert(result == "fajnarzecz")
+        assertTrue(result == "fajnarzecz")
     }
     @Test
     fun returnsCorrectConcatWhenBothEmpty() {
         val result =  UtilityFunctions.concatNextItem("", "")
-        assert(result == "")
+        assertTrue(result == "")
     }
     @Test
     fun returnsCorrectInstructions() {
         val result =  UtilityFunctions.adjustInstruction("test")
-        assert(result == "test")
+        assertTrue(result == "test")
     }
     @Test
     fun returnsCorrectInstructionsWithDot() {
         val result =  UtilityFunctions.adjustInstruction("test. A tera nowe. ")
-        assert(result == "test\nA tera nowe\n")
+        assertTrue(result == "test\nA tera nowe\n")
     }
     @Test
     fun returnsCorrectInstructionsWithSemi() {
         val result =  UtilityFunctions.adjustInstruction("test: ")
-        assert(result == "test\n")
+        assertTrue(result == "test\n")
     }
     @Test
     fun returnsCorrectInstructionsWhenEmpty() {
         val result =  UtilityFunctions.adjustInstruction("")
-        assert(result == "")
+        assertTrue(result == "")
     }
 
 
